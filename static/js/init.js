c3e37567c35db9a69b0a6c46d884857cb149386e
Joao Victor <joaovictornap@hotmail.com>
2018-03-06 14:52:23 -0300

    add arquivos first time

---
+++ static/js/init.js
@@ -0,0 +1,15 @@
+$(".button-collapse").sideNav();
+
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+    
+// (function($){
+//   $(function(){
+//
+//     $('.button-collapse').sideNav();
+//
+//   }); // fim do documento pronto
+// })(jQuery); // fim do jQuery name space
