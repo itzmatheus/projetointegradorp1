f5d38e445b3bec8e676be0466022fc77837b48bf
Matheus Jose <matheusjoselfm@gmail.com>
2018-04-13 09:34:31 -0300

    New site

---
+++ Landing Page/MaterialParallaxTemplate-gh-pages/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    // $('.modal').modal();
+    $('#modal1').modal('open');
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
