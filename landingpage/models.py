d9b9e2f6c64cb30ec1c9f90f9094484dce2b1b51
Matheus Jose <matheusjoselfm@gmail.com>
2018-04-15 20:37:03 -0300

    Add modal para canvas

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Mensagem(models.Model):
+	name = models.CharField(verbose_name="Nome completo", max_length=50, blank=True, Null=True)	
+	title = models.CharField(verbose_name="Titulo da mensagem", max_length=30)
--- static/js/init.js
+++ static/js/init.js
@@ -3,8 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    // $('.modal').modal();
-    $('#modal1').modal('open');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
